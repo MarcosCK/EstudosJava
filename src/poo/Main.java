2a48c440923ec82d646c2daac3994b3fa328de37
MarcosCK <raidenchur@gmail.com>
2023-06-12 20:53:01 -0300

    Update class main

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -5,11 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		
-		
-
-     
+		ContaBanco p1 = new ContaBanco(0, false);
+		p1.setNumConta(111);
+		p1.setDono("Marcao");
+		p1.abrirConta("CC");
+        p1.setStatus(true);
+        p1.estadoAtual();
 	}
 
 }
